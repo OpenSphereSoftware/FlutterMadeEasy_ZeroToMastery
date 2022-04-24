f3f37761d62974d43a8bc4f0b1d0fe27352366b9
Max Steffen <max.steffen@mail.com>
2022-04-24 18:57:13 +0200

    add getter and setter

---
+++ 1_dart/16_getter_setter_visibility.dart
@@ -0,0 +1,44 @@
+void main() {
+  Car car1 = Car();
+  car1.setColor = "red";
+
+  Car car2 = Car();
+  car2.setColor = "blue";
+
+  String colorFromCar1 = car1.color;
+
+  print('color from car 1: $colorFromCar1');
+
+  car1.whichColor();
+  car2.whichColor();
+
+  car1.drive();
+}
+
+class Car {
+  //! attribute
+  late String _color;
+
+  //! setter - set attribute
+  set setColor(String color) {
+    // here you can validate the input
+    this._color = color;
+  }
+
+  //! getter
+  String get color => this._color;
+
+  //! methods
+  void drive() {
+    _selectGear();
+    print("car is moving");
+  }
+
+  void _selectGear() {
+    print("Gear 1");
+  }
+
+  void whichColor() {
+    print('car color: ${this._color}');
+  }
+}
