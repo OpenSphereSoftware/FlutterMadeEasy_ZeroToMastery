d0ee250611d19b78cb434b59048d2c141f382e93
Max Berktold <max.berktold@opensphere.software>
2022-05-04 13:21:48 +0200

    added code for switch case

---
+++ 1_dart/07_switch_case.dart
@@ -0,0 +1,14 @@
+void main() {
+  final String name = "Tom";
+
+  switch (name) {
+    case "Hans":
+      print("My name is Hans!");
+      break;
+    case "Peter":
+      print("My name is Peter!");
+      break;
+    default:
+      print("I don not know my name?!");
+  }
+}
