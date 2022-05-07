f0f7066953ad45486f79eb09a34d31bd61a61c7d
Max Berktold <max.berktold@opensphere.software>
2022-05-07 15:09:03 +0200

    added code for for-each

---
+++ 1_dart/11_for_each.dart
@@ -0,0 +1,23 @@
+void main() {
+  List<int> numberList = [3, 4, 5, 6, 7, 5];
+  List<int> secondList = [];
+
+  /* for (int i = 0; i < numberList.length; i++) {
+    if (numberList[i] == 3) {
+      secondList.add(numberList[i]);
+    }
+    print('list element: ${numberList[i]}');
+    print('second list: $secondList');
+  } */
+
+  numberList.forEach((element) { 
+    element++;
+    print(element);
+    secondList.add(element);
+  });
+
+  print(numberList);
+  print(secondList);
+
+  
+}
