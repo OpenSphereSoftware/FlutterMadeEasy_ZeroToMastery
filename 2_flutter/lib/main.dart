1a835800bc2ce3d9d7adc02d6550efb0e0ac3aad
Max Steffen <max.steffen@mail.com>
2022-06-03 14:21:23 +0200

    2.4 scaffold and materialApp

--- 2_flutter/lib/main.dart
+++ 2_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(appBarTheme: const AppBarTheme(backgroundColor: Colors.amber)),
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter Basics")),
         body: const Center(
           child: Text("Hello World!"),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () => debugPrint('clicked'), child: const Icon(Icons.ac_unit)),
       ),
     );
   }
