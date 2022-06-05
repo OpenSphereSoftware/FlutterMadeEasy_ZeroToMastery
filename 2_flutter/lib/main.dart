c3ff2e251f6d0431002feb911414ae7afeba1989
Max Steffen <max.steffen@mail.com>
2022-06-05 14:42:01 +0200

    2.6 column, sizedBox, padding

--- 2_flutter/lib/main.dart
+++ 2_flutter/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,14 +13,39 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(appBarTheme: const AppBarTheme(backgroundColor: Colors.amber)),
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter Basics")),
-        body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-            child: const Center(
-              child: Text("Hello World!", style: TextStyle(color: Colors.red, fontSize: 20)),
-            ),
+        body: Container(
+          color: Colors.pink,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.green,
+                child: const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      'First Column child',
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                    child: Text("Hello World!", style: TextStyle(color: Colors.red, fontSize: 20)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text('Last Column child'),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
