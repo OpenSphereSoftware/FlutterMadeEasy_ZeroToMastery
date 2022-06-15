ad7431850e2870688ebfac149590c71bd3d82061
Max Steffen <max.steffen@mail.com>
2022-06-15 10:55:30 +0200

    2.8 asset, image, stack

--- 2_flutter/lib/main.dart
+++ 2_flutter/lib/main.dart
@@ -69,6 +69,28 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              Stack(
+                children: [
+                  SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset(
+                        'assets/max.jpeg',
+                        fit: BoxFit.cover,
+                      )),
+                  const Positioned(
+                    top: 80,
+                    left: 20,
+                    child: Text('Max Steffen'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://static.wixstatic.com/media/e38214_2415b962d0244310bb630e9cb6ac7010~mv2.jpg/v1/fill/w_388,h_372,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/IMG_5274_edited_edited_edited_edited.jpg'))),
               const SizedBox(
                 height: 40,
               ),
