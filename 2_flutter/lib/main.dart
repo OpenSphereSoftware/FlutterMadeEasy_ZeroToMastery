5819da6243dc9aa0e094938c351068542ff5018f
Max Berktold <max.berktold@opensphere.software>
2022-06-26 16:08:23 +0200

    2.11 layout builder

--- 2_flutter/lib/main.dart
+++ 2_flutter/lib/main.dart
@@ -110,16 +110,26 @@ class MyApp extends StatelessWidget {
                 const SizedBox(
                   height: 40,
                 ),
-                Builder(builder: (context) {
-                  return Container(
-                    color: Colors.green,
-                    width: MediaQuery.of(context).size.width,
-                    height: 100,
-                    child: const Center(
-                      child: Text("MediaQuery Example"),
-                    ),
-                  );
-                })
+                Container(
+                  height: 300,
+                  width: 300,
+                  color: Colors.yellow,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return Center(
+                      child: Container(
+                        color: Colors.blue,
+                        width: constraints.maxWidth * 0.8,
+                        height: constraints.maxHeight / 2,
+                        child: const Center(
+                          child: Text("Layout Builder Example"),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
               ],
             ),
           ),
