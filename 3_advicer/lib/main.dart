82fa60cf1383578864272f69b52f8f1089a91b80
Max Berktold <max.berktold@opensphere.software>
2022-09-28 18:18:26 +0200

    added basic project

---
+++ 3_advicer/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:advicer/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '3_application/core/services/theme_service.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const Placeholder(),
+      );
+    });
+  }
+}
---
+++ 3_advicer/lib/theme.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  // *****************
+  // static colors
+  // *****************
+  static final Color _lightPrimaryColor = Colors.blueGrey.shade50;
+  static final Color _lightPrimaryVariantColor = Colors.blueGrey.shade800;
+  static final Color _lightOnPrimaryColor = Colors.blueGrey.shade200;
+  static const Color _lightTextColorPrimary = Colors.black;
+  static const Color _appbarColorLight = Colors.blue;
+
+  static final Color _darkPrimaryColor = Colors.blueGrey.shade900;
+  static const Color _darkPrimaryVariantColor = Colors.black;
+  static final Color _darkOnPrimaryColor = Colors.blueGrey.shade300;
+  static const Color _darkTextColorPrimary = Colors.white;
+  static final Color _appbarColorDark = Colors.blueGrey.shade800;
+
+  static const Color _iconColor = Colors.white;
+
+  static const Color _accentColor = Color.fromRGBO(74, 217, 217, 1);
+
+  // *****************
+  // Text Style - light
+  // *****************
+  static const TextStyle _lightHeadingText = TextStyle(
+      color: _lightTextColorPrimary,
+      fontFamily: "Rubik",
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
+
+  static const TextStyle _lightBodyText = TextStyle(
+      color: _lightTextColorPrimary,
+      fontFamily: "Rubik",
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.bold,
+      fontSize: 16);
+
+  static const TextTheme _lightTextTheme = TextTheme(
+    headline1: _lightHeadingText,
+    bodyText1: _lightBodyText,
+  );
+
+  // *****************
+  // Text Style - dark
+  // *****************
+  static final TextStyle _darkThemeHeadingTextStyle =
+      _lightHeadingText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextStyle _darkThemeBodyeTextStyle =
+      _lightBodyText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextTheme _darkTextTheme = TextTheme(
+    headline1: _darkThemeHeadingTextStyle,
+    bodyText1: _darkThemeBodyeTextStyle,
+  );
+
+  // *****************
+  // Theme light/dark
+  // *****************
+
+  static final ThemeData lightTheme = ThemeData(
+      scaffoldBackgroundColor: _lightPrimaryColor,
+      appBarTheme: const AppBarTheme(
+          color: _appbarColorLight,
+          iconTheme: IconThemeData(color: _iconColor)),
+      bottomAppBarColor: _appbarColorLight,
+      colorScheme: ColorScheme.light(
+        primary: _lightPrimaryColor,
+        onPrimary: _lightOnPrimaryColor,
+        secondary: _accentColor,
+        primaryContainer: _lightPrimaryVariantColor,
+      ),
+      textTheme: _lightTextTheme);
+
+  static final ThemeData darkTheme = ThemeData(
+      scaffoldBackgroundColor: _darkPrimaryColor,
+      appBarTheme: AppBarTheme(
+          color: _appbarColorDark,
+          iconTheme: const IconThemeData(color: _iconColor)),
+      bottomAppBarColor: _appbarColorDark,
+      colorScheme: ColorScheme.dark(
+        primary: _darkPrimaryColor,
+        secondary: _accentColor,
+        onPrimary: _darkOnPrimaryColor,
+        primaryContainer: _darkPrimaryVariantColor,
+      ),
+      textTheme: _darkTextTheme);
+}
