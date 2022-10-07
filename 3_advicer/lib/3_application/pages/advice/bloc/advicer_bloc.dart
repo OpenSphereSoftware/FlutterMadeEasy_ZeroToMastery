78aae0d0e555582b9f5359c551b165dd74c3ec9d
Max Berktold <max.berktold@opensphere.software>
2022-10-07 18:42:10 +0200

    added bloc provider

--- 3_advicer/lib/3_application/pages/advice/bloc/advicer_bloc.dart
+++ 3_advicer/lib/3_application/pages/advice/bloc/advicer_bloc.dart
@@ -13,8 +13,8 @@ class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
       debugPrint('fake get advice triggered');
       await Future.delayed(const Duration(seconds: 3), () {});
       debugPrint('got advice');
-      emit(AdvicerStateLoaded(advice: 'fake advice to test bloc'));
-      //emit(AdvicerStateError(message: 'error message'));
+      //emit(AdvicerStateLoaded(advice: 'fake advice to test bloc'));
+      emit(AdvicerStateError(message: 'error message'));
     });
   }
 }
--- 3_advicer/lib/main.dart
+++ 3_advicer/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        home: const AdvicerPage(),
+        home: const AdvicerPageWrapperProvider(),
       );
     });
   }
