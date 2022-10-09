4656db586ec8beb13cd31a54563fddcad3563ce2
Max Berktold <max.berktold@opensphere.software>
2022-10-09 23:49:51 +0200

    added equatable package

--- 3_advicer/lib/2_application/pages/advice/bloc/advicer_event.dart
+++ 3_advicer/lib/2_application/pages/advice/bloc/advicer_event.dart
@@ -1,6 +1,9 @@
 part of 'advicer_bloc.dart';
 
 @immutable
-abstract class AdvicerEvent {}
+abstract class AdvicerEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class AviceRequestedEvent extends AdvicerEvent {}
