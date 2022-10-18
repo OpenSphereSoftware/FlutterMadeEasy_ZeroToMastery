e4c029ee57b7ae2dd7a77f9af8ccde9e5035859e
Max Berktold <max.berktold@opensphere.software>
2022-10-18 10:17:21 +0200

    added failures and dartz

--- 3_advicer/lib/1_domain/repositories/advice_repo.dart
+++ 3_advicer/lib/1_domain/repositories/advice_repo.dart
@@ -1,7 +1,8 @@
-
-
 import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import '../failures/failures.dart';
 
 abstract class AdviceRepo {
-  Future<AdviceEntity> getAdviceFromDatasource();
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDatasource();
 }
--- 3_advicer/lib/2_application/pages/advice/cubit/advicer_cubit.dart
+++ 3_advicer/lib/2_application/pages/advice/cubit/advicer_cubit.dart
@@ -1,9 +1,14 @@
-import 'package:advicer/1_domain/entities/advice_entity.dart';
 import 'package:advicer/1_domain/usecases/advice_usecases.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../1_domain/failures/failures.dart';
+
 part 'advicer_state.dart';
+
+const generalFailureMessage = 'Ups, something gone wrong. Please try again!';
+const serverFailureMessage = 'Ups, API Error. please try again!';
+const cacheFailureMessage = 'Ups, chache failed. Please try again!';
 
 class AdvicerCubit extends Cubit<AdvicerCubitState> {
   AdvicerCubit() : super(AdvicerInitial());
@@ -12,8 +17,20 @@ class AdvicerCubit extends Cubit<AdvicerCubitState> {
 
   void adviceRequested() async {
     emit(AdvicerStateLoading());
-    final AdviceEntity advice = await adviceUseCases.getAdvice();
-    emit(AdvicerStateLoaded(advice: advice.advice));
-    //emit(const AdvicerStateError(message: 'error message'));
+    final failureOrAdvice = await adviceUseCases.getAdvice();
+    failureOrAdvice.fold(
+        (failure) => emit(AdvicerStateError(message: _mapFailureToMessage(failure))),
+        (advice) => emit(AdvicerStateLoaded(advice: advice.advice)));
+  }
+
+  String _mapFailureToMessage(Failure failure){
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
