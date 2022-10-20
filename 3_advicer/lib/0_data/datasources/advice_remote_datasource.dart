bd5edd5cd93097410d6e689042ff42bb04677647
Max Berktold <max.berktold@opensphere.software>
2022-10-20 18:19:50 +0200

    added remote datasource

---
+++ 3_advicer/lib/0_data/datasources/advice_remote_datasource.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/advice_model.dart';
+
+abstract class AdviceRemoteDatasource {
+  /// requests a random advice from api
+  /// returns [AdviceModel] if successfull
+  /// throws a server-Exception if status code is not 200
+  Future<AdviceModel> getRandomAdviceFromApi();
+}
+
+class AdviceRemoteDatasourceImpl implements AdviceRemoteDatasource {
+  final client = http.Client();
+
+  @override
+  Future<AdviceModel> getRandomAdviceFromApi() async {
+    final response = await client.get(
+        Uri.parse('https://api.flutter-community.com/api/v1/advice'),
+        headers: {
+          'content-type': 'application/json ',
+        },);
+    final responseBody = json.decode(response.body);
+    return AdviceModel.fromJson(responseBody);
+  }
+}
---
+++ 3_advicer/lib/0_data/repositories/advice_repo_impl.dart
@@ -0,0 +1,15 @@
+import 'package:advicer/0_data/datasources/advice_remote_datasource.dart';
+import 'package:advicer/1_domain/failures/failures.dart';
+import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:advicer/1_domain/repositories/advice_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class AdviceRepoImpl implements AdviceRepo {
+  final AdviceRemoteDatasource adviceRemoteDatasource =
+      AdviceRemoteDatasourceImpl();
+  @override
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDatasource() async {
+    final result = await adviceRemoteDatasource.getRandomAdviceFromApi();
+    return right(result);
+  }
+}
