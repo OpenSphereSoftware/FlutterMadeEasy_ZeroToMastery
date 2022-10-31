971158bd12b1dc3122baf2ef4ee65c1a8c39783c
Max Steffen <max.steffen@mail.com>
2022-10-31 13:26:32 +0100

    update config

--- 3_advicer/test/flutter_test_config.dart
+++ 3_advicer/test/flutter_test_config.dart
@@ -12,29 +12,29 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async {
 
     // ASSET FONTS
     // loads a font that is bundled in your package (Open Sans for example)
-    final fontVariable = rootBundle.load('lib/fonts/Open_Sans/OpenSans-VariableFont.ttf');
-    final fontItalic = rootBundle.load('lib/fonts/Open_Sans/OpenSans-Italic-VariableFont.ttf');
+    // final fontVariable = rootBundle.load('lib/fonts/Open_Sans/OpenSans-VariableFont.ttf');
+    // final fontItalic = rootBundle.load('lib/fonts/Open_Sans/OpenSans-Italic-VariableFont.ttf');
 
     // here you maybe need to load more types, when you have a specific bold style as well
-    final fontLoader = FontLoader('OpenSans')
-      ..addFont(fontVariable)
-      ..addFont(fontItalic);
+    // final fontLoader = FontLoader('OpenSans')
+    //   ..addFont(fontVariable)
+    //   ..addFont(fontItalic);
 
-    await fontLoader.load();
+    // await fontLoader.load();
 
     // PACKAGE FONTS
     // loads a font that is bundled in a different package, here for example from the fluent icons package (https://pub.dev/packages/fluentui_system_icons)
-    final fluentRegularIcon = rootBundle.load('packages/fluentui_system_icons/fonts/FluentSystemIcons-Regular.ttf');
-    final fluentFilledIcon = rootBundle.load('packages/fluentui_system_icons/fonts/FluentSystemIcons-Filled.ttf');
+    // final fluentRegularIcon = rootBundle.load('packages/fluentui_system_icons/fonts/FluentSystemIcons-Regular.ttf');
+    // final fluentFilledIcon = rootBundle.load('packages/fluentui_system_icons/fonts/FluentSystemIcons-Filled.ttf');
 
     // there is no need to write extension methods or create specific themes, you only need to add the packages/package_name to the font name.
-    final fluentRegularLoader = FontLoader('packages/fluentui_system_icons/FluentSystemIcons-Regular')
-      ..addFont(fluentRegularIcon);
-    final fluentFilledLoader = FontLoader('packages/fluentui_system_icons/FluentSystemIcons-Filled')
-      ..addFont(fluentFilledIcon);
+    // final fluentRegularLoader = FontLoader('packages/fluentui_system_icons/FluentSystemIcons-Regular')
+    //   ..addFont(fluentRegularIcon);
+    // final fluentFilledLoader = FontLoader('packages/fluentui_system_icons/FluentSystemIcons-Filled')
+    //   ..addFont(fluentFilledIcon);
 
-    await fluentRegularLoader.load();
-    await fluentFilledLoader.load();
+    // await fluentRegularLoader.load();
+    // await fluentFilledLoader.load();
 
     // MATERIAL FONTS
     // loads the material icon font and roboto font
