570ffcde940f7078d477672e43b6218f53ceacee
Max Steffen <max.steffen@mail.com>
2022-10-31 12:33:41 +0100

    add golden test for custom button and font loading

--- 3_advicer/lib/2_application/pages/advice/widgets/custom_button.dart
+++ 3_advicer/lib/2_application/pages/advice/widgets/custom_button.dart
@@ -14,7 +14,10 @@ class CustomButton extends StatelessWidget {
         elevation: 20,
         borderRadius: BorderRadius.circular(15),
         child: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: themeData.colorScheme.secondary),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: onTap == null ? themeData.colorScheme.errorContainer : themeData.colorScheme.secondary,
+          ),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
             child: Text(
