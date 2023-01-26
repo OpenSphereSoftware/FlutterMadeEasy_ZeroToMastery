296103ff47ab23fd4130d0ac7328fab40dd8c96a
Max Steffen <max.steffen@mail.com>
2023-01-26 07:45:35 +0100

    fix unit test

--- 3_advicer/test/0_data/datasources/advice_remote_datasource_test.dart
+++ 3_advicer/test/0_data/datasources/advice_remote_datasource_test.dart
@@ -52,7 +52,7 @@ void main() {
         when(mockClient.get(
           Uri.parse('https://api.flutter-community.com/api/v1/advice'),
           headers: {
-            'content-type': 'application/json ',
+            'content-type': 'application/json',
           },
         )).thenAnswer((realInvocation) => Future.value(Response(responseBody, 200)));
 
