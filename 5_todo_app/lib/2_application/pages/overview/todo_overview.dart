bf878b9df2cb64bd7454b0ea4fc82b26ff5d2809
Max Steffen <max.steffen@mail.com>
2023-01-27 11:38:25 +0100

    add gesture detecure

--- 5_todo_app/lib/2_application/pages/overview/todo_overview.dart
+++ 5_todo_app/lib/2_application/pages/overview/todo_overview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/1_domain/entities/Todo.dart';
 import 'package:todo_app/1_domain/entities/todo_color.dart';
+import 'package:todo_app/2_application/pages/home/home_page.dart';
 import 'package:todo_app/main.dart';
 
 class TodoOverview extends StatelessWidget {
@@ -12,10 +13,13 @@ class TodoOverview extends StatelessWidget {
 
     return ListView.builder(
       itemCount: todos.length,
-      itemBuilder: (context, index) => Card(
-        color: todos[index].color.color,
-        child: ListTile(
-          title: Text(todos[index].title),
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () => debugPrint('select ${todos[index].id}'),
+        child: Card(
+          color: todos[index].color.color,
+          child: ListTile(
+            title: Text(todos[index].title),
+          ),
         ),
       ),
     );
