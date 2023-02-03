688fadf8a0f721323c635acc8d2e9aa62d915ed9
Max Steffen <max.steffen@mail.com>
2023-02-03 10:57:32 +0100

    remove error from file

--- 5_todo_app/lib/1_domain/use_cases/load_overview_collections.dart
+++ 5_todo_app/lib/1_domain/use_cases/load_overview_collections.dart
@@ -2,7 +2,7 @@ import 'package:either_dart/either.dart';
 import 'package:todo_app/1_domain/entities/todo_collection.dart';
 import 'package:todo_app/1_domain/failures/failures.dart';
 import 'package:todo_app/1_domain/repositories/todo_repository.dart';
-import 'package:todo_app/core/use_case/use_case.dart';´
+import 'package:todo_app/core/use_case/use_case.dart';
 
 class LoadOverviewCollections implements UseCase<void, NoParams> {
   const LoadOverviewCollections({required this.toDoRepository});
