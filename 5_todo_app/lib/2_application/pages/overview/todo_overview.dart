0e31fa2984fcf2643bc9fc39b3eec40544d58d2f
Max Steffen <max.steffen@mail.com>
2023-02-12 09:32:45 +0100

    add main method to start app in memory mode

--- 5_todo_app/lib/2_application/pages/overview/todo_overview.dart
+++ 5_todo_app/lib/2_application/pages/overview/todo_overview.dart
@@ -11,7 +11,6 @@ class ToDoOverviewProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('ToDoOverviewProvider build');
     return BlocProvider(
       create: (context) => ToDoOverviewCubit(
         loadOverviewCollections: LoadOverviewCollections(
@@ -28,7 +27,6 @@ class ToDoOverview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('ToDoOverview build');
     return BlocBuilder<ToDoOverviewCubit, ToDoOverviewState>(
       builder: (context, state) {
         if (state is ToDoOverviewLoadingState) {
