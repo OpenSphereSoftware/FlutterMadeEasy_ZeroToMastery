2671b1e00ff7f2e067d925f67ebbbe0a93081992
Max Steffen <max.steffen@mail.com>
2023-02-16 12:57:32 +0100

    fix color issue on create collection

--- 5_todo_app/lib/2_application/pages/create_todo_collection/bloc/create_todo_collection_cubit.dart
+++ 5_todo_app/lib/2_application/pages/create_todo_collection/bloc/create_todo_collection_cubit.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/1_domain/entities/todo_collection.dart';
+import 'package:todo_app/1_domain/entities/todo_color.dart';
 import 'package:todo_app/1_domain/use_cases/add_todo_collection.dart';
 import 'package:todo_app/core/use_case/use_case.dart';
 
@@ -25,6 +26,13 @@ class CreateToDoCollectionCubit extends Cubit<CreateToDoCollectionState> {
   void submit() async {
     emit(state.copyWith(isSubmitting: true));
 
-    await addToDoCollection.call(ToDoCollectionParams(collection: ToDoCollection.empty().copyWith(title: state.title)));
+    await addToDoCollection.call(
+      ToDoCollectionParams(
+        collection: ToDoCollection.empty().copyWith(
+          title: state.title,
+          color: TodoColor(colorIndex: int.tryParse(state.color ?? '') ?? 0),
+        ),
+      ),
+    );
   }
 }
