bccfad5eb0f56e727dfc07ee502eff01a78dbf75
Max Steffen <max.steffen@mail.com>
2023-03-11 10:54:13 +0100

    add missing routes

--- 5_todo_app/lib/2_application/core/routes.dart
+++ 5_todo_app/lib/2_application/core/routes.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/2_application/core/go_router_observer.dart';
+import 'package:todo_app/2_application/pages/dashboard/dashboard_page.dart';
 import 'package:todo_app/2_application/pages/home/home_page.dart';
 import 'package:todo_app/2_application/pages/settings/settings_page.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
 
+const String _basePath = '/home';
+
 final routes = GoRouter(
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home/dashboard',
+  initialLocation: '$_basePath/${DasboardPage.pageConfig.name}',
   observers: [GoRouterObserver()],
   routes: [
     GoRoute(
-      path: '/home/settings',
+      name: SettingsPage.pageConfig.name,
+      path: '$_basePath/${SettingsPage.pageConfig.name}',
       builder: (context, state) {
         return const SettingsPage();
       },
@@ -23,7 +27,8 @@ final routes = GoRouter(
       builder: (context, state, child) => child,
       routes: [
         GoRoute(
-          path: '/home/:tab',
+          name: HomePage.pageConfig.name,
+          path: '$_basePath/:tab',
           builder: (context, state) => HomePage(
             key: state.pageKey,
             tab: state.params['tab']!,
