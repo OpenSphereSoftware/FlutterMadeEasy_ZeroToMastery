bc7a4163bccec9a8e2d847563eb0dffcadfcdd4c
Max Steffen <max.steffen@mail.com>
2023-10-06 13:00:13 +0200

    fix lint warning

--- 5_todo_app/lib/0_data/data_sources/local/hive_local_data_source.dart
+++ 5_todo_app/lib/0_data/data_sources/local/hive_local_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:todo_app/0_data/data_sources/interfaces/todo_local_data_source_interface.dart';
