e95d439ce219c9d8023ad4430520c80acbb0c835
Max Steffen <max.steffen@mail.com>
2023-11-01 11:15:07 +0100

    add streams to main

--- 1_dart/20_streams.dart
+++ 1_dart/20_streams.dart
@@ -1,27 +1,41 @@
 import 'dart:async';
 
-void main(){
-  Stream numberStream = NumberGenerator().getStream.asBroadcastStream();
+void main() {
+  final numberGenerator = NumberGenerator();
+  final numberStream = numberGenerator.getStream.asBroadcastStream();
   //! broadcast -> can handle multiple subscribers
 
   StreamSubscription sub1 = numberStream.listen((event) {
-    print(event);
+    print('sub1: $event');
+  });
+
+  StreamSubscription sub2 = numberStream.listen((event) {
+    print('sub2: ${event * event}');
+  });
+
+  Future.delayed(const Duration(seconds: 5), () {
+    sub1.cancel();
+    sub2.cancel();
   });
 }
 
 class NumberGenerator {
-  int _counter = 0;
-
-  StreamController<int> _controller = StreamController<int>();
-
-  Stream<int> get getStream => _controller.stream;
-
-  NumberGenerator(){
-    final timer = Timer.periodic(Duration(seconds: 1), (timer) { 
-      _controller.sink.add(_counter);
+  NumberGenerator() {
+    final timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _controller.add(_counter);
+      // _controller.sink.add(_counter);
       _counter++;
     });
 
-    Future.delayed(Duration(seconds: 10), () => timer.cancel());
+    Future.delayed(const Duration(seconds: 10), () {
+      timer.cancel();
+      _controller.close();
+    });
   }
+
+  int _counter = 0;
+
+  final StreamController<int> _controller = StreamController<int>();
+
+  Stream<int> get getStream => _controller.stream;
 }
