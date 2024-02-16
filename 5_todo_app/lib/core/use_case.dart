3dd1b996fbcb5e6f3b1506936d621139ca2868df
Max Steffen <max.steffen@mail.com>
2024-02-16 11:02:38 +0100

    5.6.2 add use case and mock

---
+++ 5_todo_app/lib/core/use_case.dart
@@ -0,0 +1,14 @@
+import 'package:either_dart/either.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todo_app/1_domain/failures/failures.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+abstract class Params extends Equatable {}
+
+class NoParams extends Params {
+  @override
+  List<Object?> get props => [];
+}
