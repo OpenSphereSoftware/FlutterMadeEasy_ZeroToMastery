370e0de8937370ea6ff8fc5e887567589cdad866
Max Steffen <max.steffen@mail.com>
2024-04-19 10:38:55 +0200

    5.11.1 add navigation cubit and use it on selected tiles

--- 5_todo_app/lib/2_application/pages/overview/view_states/todo_overview_loaded.dart
+++ 5_todo_app/lib/2_application/pages/overview/view_states/todo_overview_loaded.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/1_domain/entities/todo_collection.dart';
 import 'package:todo_app/2_application/pages/detail/todo_detail_page.dart';
+import 'package:todo_app/2_application/pages/home/bloc/navigation_todo_cubit.dart';
 
 class ToDoOverviewLoaded extends StatelessWidget {
   const ToDoOverviewLoaded({
@@ -26,6 +28,8 @@ class ToDoOverviewLoaded extends StatelessWidget {
           iconColor: item.color.color,
           selectedColor: item.color.color,
           onTap: () {
+            context.read<NavigationToDoCubit>().selectedToDoCollectionChanged(item.id);
+
             if (Breakpoints.small.isActive(context)) {
               context.pushNamed(
                 ToDoDetailPage.pageConfig.name,
